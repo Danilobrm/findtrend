0396e89fe135912440984bb34d72d4a96d9a6371
danilobrm <danilobr.web@gmail.com>
2023-05-03 13:28:18 -0300

    file javascript missed

---
+++ index.js
@@ -0,0 +1,23 @@
+const toggleRetangle = document.querySelector(".turn-retangle");
+const navMenu = document.querySelector(".nav-bar");
+const hamburguer = document.querySelector(".hamburguer");
+const bars = document.querySelectorAll(".bar");
+const navItem = document.querySelectorAll(".nav-item");
+
+let active = false;
+function toggleOption() {
+  toggleRetangle.style.justifyContent = active ? "flex-start" : "flex-end";
+  active = !active;
+}
+
+function openMenu() {
+  navMenu.classList.toggle("active");
+  hamburguer.classList.toggle("active");
+  bars.forEach((bar) => bar.classList.toggle("active"));
+}
+
+navItem.forEach((item) =>
+  item.addEventListener("click", () => {
+    openMenu();
+  })
+);
